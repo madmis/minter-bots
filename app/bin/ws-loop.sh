41a93ff02d0c149ed6671dd138c1685551bb5d51
Dmytro Machin <dmitriy.m@payop.com>
2021-05-23 18:54:16 +0300

    bash

--- app/bin/ws-loop.sh
+++ app/bin/ws-loop.sh
@@ -1,3 +1,3 @@
 #bash
-for i in {1..1000000}; do ./console app:ws:subscribe -a 1000 -a 2000 --req-delay=0; done;
+for i in {1..1000000}; do /var/www/ccbip/bin/console app:ws:subscribe -a 1000 -a 2000 --req-delay=0; done;
 
